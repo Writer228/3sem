f9dcd88e7a8929b305c29937986b31e1590d3314
VladMiryaha <vlad.miryaha@gmail.com>
2016-10-16 03:07:56 +0300

    fix it

--- dispersia.c
+++ dispersia.c
@@ -3,7 +3,6 @@
  * При N = 3 программа работает не правильно и в течение 20 секунд.
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,8 +12,14 @@
 #include <string.h>
 #include <sys/types.h>
 
-# define N 1
+# define N 2
 
+/*
+ * FIXIT:
+ * Для 10^8 нужна отдельная константа.
+ * И странно, что такой большой массив получилось выделить на стеке...
+ * Обычно стараются на куче столько много памяти просить. 
+ */
 double value[100000000];
 double sum[N];
 double sum_square[N];
@@ -27,6 +32,9 @@ void* my_thread(void *dummy)
 	sum[k] = 0;
 	sum_square[k] = 0;
 	
+  /*
+    sum[k] += value[i + k * size];
+   */
 	while(i < size)
 	{
 		sum[k] = sum[k] + value[i + k*size];	
@@ -43,7 +51,12 @@ int main()
 	double a = 0, b = 0;
 	double average = 0, average_square = 0;
 	unsigned long long int i = 0;
-	
+  
+  /*
+   * FIXIT:
+   * Не надо лепить while, где "направшивается" обычный for.
+   * Относится ко всем while ниже.
+   */
 	while (i < 100000000)
 	{
 		value[i] = random() %7;
@@ -60,6 +73,11 @@ int main()
 			printf("Can't create thread");
 		}
 		i++;
+    /*
+     * FIXIT:
+     * Если у вас операции i++ выполнится быстрее, чем int k = *(long int*) dummy;
+     * то результат будет неверный. В презентации к семинару 5 есть отдельный слайд, посвященный этой проблеме.
+     */
 	}
 	i = 0;
 	
@@ -83,7 +101,9 @@ int main()
 	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 	
 	printf("average value = %f \ndispersion = %f\ntime spent = %f\n ", average, b, time_spent);
+  /*
+   * http://stackoverflow.com/questions/19616895/increasing-pthreads-thread-count-has-no-effect-on-speed
+   * Я забыл про эту особенность clock'a, поэтому ускорение скорее всего вы не получили.
+   */
 	return 0;
-	
-	
 }
